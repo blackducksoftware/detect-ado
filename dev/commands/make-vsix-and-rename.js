56adf59bd7007a69a2064f62277df7633b90f97b
Jordan Piscitelli <jordan.r.piscitelli@gmail.com>
2018-02-06 18:41:43 -0500

    Update make-vsix-and-rename.js
    Switching to rename in the lib.

--- dev/commands/make-vsix-and-rename.js
+++ dev/commands/make-vsix-and-rename.js
@@ -1,4 +1,4 @@
-maker.buildVsix(function (err, result){
+maker.buildVsixAndRename(function (err, result){
     if (err){
         throw err;
     }else{
@@ -6,14 +6,5 @@ maker.buildVsix(function (err, result){
         console.log(`Vsix: ${result.vsixPath}`)
         console.log(`Id: ${result.packageId}`)
         console.log(`Version: ${result.packageVersion}`)
- 
-        maker.rename(result.vsixPath, result.packageId, result.packageVersion, function (err, newVsix){
-            if (err){
-                throw error;
-            }else{
-                console.log("Renamed!");
-                console.log(`Final Vsix: ${newVsix}`);
-            }
-        });
     }
 });
