92cdfda8358df38f90cbf88e81cafbbe0f87bb54
Brian Mandel <bmandel@blackducksoftware.com>
2020-12-02 16:49:12 -0500

    Added an env var for setting a download location for detect jars. The script will now use an existing jar in that location or download a jar and put it there.

--- tasks/synopsys-detect-task/ts/DetectSetup.ts
+++ tasks/synopsys-detect-task/ts/DetectSetup.ts
@@ -2,6 +2,7 @@ import {IBlackduckConfiguration} from "./model/IBlackduckConfiguration";
 import {IDetectConfiguration} from "./model/IDetectConfiguration";
 import {logger} from "./DetectLogger";
 import * as task from "azure-pipelines-task-lib";
+import path from "path";
 
 export class DetectSetup {
     createEnvironmentWithVariables(blackduckConfiguration: IBlackduckConfiguration, detectConfiguration: IDetectConfiguration): any {
@@ -25,6 +26,10 @@ export class DetectSetup {
         // Set according to the Powershell script
         env['DETECT_EXIT_CODE_PASSTHRU'] = "1"
 
+        const toolDirectory = task.getVariable('Agent.ToolsDirectory')
+        if (toolDirectory) {
+            env["DETECT_JAR_DOWNLOAD_DIR"] = path.resolve(toolDirectory, "detect")
+        }
         env['DETECT_SOURCE_PATH'] = task.getVariable('BUILD_SOURCESDIRECTORY')
 
         return env
