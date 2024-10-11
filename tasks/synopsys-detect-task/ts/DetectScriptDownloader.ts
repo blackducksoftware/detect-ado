f0b46462ddcdcba63e5400bcfa181be912ba2fdb
devmehtasynopsys <devmehta@synopsys.com>
2024-10-11 12:27:18 -0400

    Separate out async functions

--- tasks/synopsys-detect-task/ts/DetectScriptDownloader.ts
+++ tasks/synopsys-detect-task/ts/DetectScriptDownloader.ts
@@ -9,42 +9,28 @@ import {PathResolver} from './PathResolver';
 export class DetectScriptDownloader {
     static readonly DETECT_DOWNLOAD_URL = 'https://detect.blackduck.com'
     static readonly DETECT_DOWNLOAD_FALLBACK_URL = 'https://detect.synopsys.com'
-    private static downloadLink: string;
 
     private constructor() {}
 
-    static async downloadScript(proxyInfo: IProxyInfo | undefined, scriptName: string, scriptDirectory: string): Promise<boolean> {
+    static async downloadScript(proxyInfo: IProxyInfo | undefined, scriptName: string, scriptDirectory: string, isBlackDuckAccessible: boolean): Promise<boolean> {
         logger.info('Downloading detect script.')
         if (!fileSystem.existsSync(scriptDirectory)) {
             fileSystem.mkdirSync(scriptDirectory, {recursive: true})
         }
         logger.info("Hello all")
 
+        let downloadLink:string
+        if(isBlackDuckAccessible) {
+            downloadLink = this.getFullDownloadUrl(scriptName)
+        } else {
+            downloadLink = this.getFullFallbackDownloadUrl(scriptName)
+        }
         const filePath: string = PathResolver.combinePathSegments(scriptDirectory, scriptName)
         const writer: WriteStream = fileSystem.createWriteStream(filePath)
         const axios: AxiosInstance = this.createAxiosAgent(proxyInfo)
 
-        logger.info(filePath, writer)
-
-        try {
-            logger.info("In try block")
-            const bdResponse = await axios.get(DetectScriptDownloader.DETECT_DOWNLOAD_URL)
-
-            if(bdResponse.status >= 200 && bdResponse.status <= 399) {
-                logger.info("In If block")
-                this.downloadLink = this.getFullDownloadUrl(scriptName);
-            } else {
-                logger.info("In else block")
-                logger.warn("https://detect.blackduck.com responded with an unexpected status code " + bdResponse.status + ". Please allow access through your firewall as https://sig-repo.synopsys.com will be shutdown at the end of February 2025.")
-                this.downloadLink = this.getFullFallbackDownloadUrl(scriptName)
-            }
-        } catch (error) {
-            logger.warn("https://detect.blackduck.com is inaccessible from this machine. Please allow access through your firewall as https://sig-repo.synopsys.com will be shutdown at the end of February 2025.")
-            this.downloadLink = this.getFullFallbackDownloadUrl(scriptName)
-        }
-
         const response = await axios({
-            url: this.downloadLink,
+            url: downloadLink,
             method: 'GET',
             responseType: 'stream'
         })
@@ -83,5 +69,9 @@ export class DetectScriptDownloader {
 
     private static getFullFallbackDownloadUrl(scriptName: string): string {
         return `${DetectScriptDownloader.DETECT_DOWNLOAD_FALLBACK_URL}/${scriptName}`
+    }
+
+    static async isBlackDuckUrlAccessible(): Promise<boolean> {
+        return false
     }
 }
